985c84c8676732138941f137c1ebd57852bd447b
AdityaMaharshi <snaas.aditya@gmail.com>
2016-04-28 16:49:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var Botkit =require('botkit)
+var Botkit =require('botkit')
 
-var accessToken = process.enc.FACEBOOK_PAGE_ACCESS_TOKEN
+var accessToken = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
 var verifyToken = process.env.FACEBOOK_VERIFY_TOKEN
 var port = process.env.PORT
 
-if(!accessToken) throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required but missing')
-if(!verifyToken) throw new Error('FACEBOOK_VERIFY_TOKEN is required but missing')
-if(!port) new Error('PORT is required but missing')
+if (!accessToken) throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required but missing')
+if (!verifyToken) throw new Error('FACEBOOK_VERIFY_TOKEN is required but missing')
+if(!port) throw new Error('PORT is required but missing')
 
 var controller = Botkit.facebookbot({
 	access_token: accessToken,
@@ -14,11 +14,10 @@ var controller = Botkit.facebookbot({
 })
 
 var bot =controller.spawn()
-controller.setupWebserver(port, function(err, webserver)){
+controller.setupWebserver(port, function(err, webserver){
 	if (err) return console.log(err)
 	controller.createWebhookEndpoints(webserver, bot, function(){
-		console.log('Read Player 1')
-	})
+		console.log('Read Player 1')})
 })
 
 controller.hears(['hello','hi'],'message_received', function (bot, message){
@@ -45,8 +44,9 @@ controller.hears(['hello','hi'],'message_received', function (bot, message){
 				]		
 			}
 		}
-	}
+	})
 })
+
 
 controller.on('facebook_postback',function(bot,message){
 	switch(message.payload){

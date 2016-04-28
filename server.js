2875cc1037f95a4c1495dbb422b0a3e9aefb0b5e
AdityaMaharshi <snaas.aditya@gmail.com>
2016-04-28 16:52:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var accessToken = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
 var verifyToken = process.env.FACEBOOK_VERIFY_TOKEN
 var port = process.env.PORT
 
-if (!accessToken) throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required but missing')
-if (!verifyToken) throw new Error('FACEBOOK_VERIFY_TOKEN is required but missing')
+if(!accessToken) throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required but missing')
+if(!verifyToken) throw new Error('FACEBOOK_VERIFY_TOKEN is required but missing')
 if(!port) throw new Error('PORT is required but missing')
 
 var controller = Botkit.facebookbot({
@@ -13,7 +13,7 @@ var controller = Botkit.facebookbot({
 	verify_token: verifyToken
 })
 
-var bot =controller.spawn()
+var bot = controller.spawn()
 controller.setupWebserver(port, function(err, webserver){
 	if (err) return console.log(err)
 	controller.createWebhookEndpoints(webserver, bot, function(){
